3efe6fec948e29df49014e4b9d62abfc0a725311
alfredd25 <alfredbiju7@gmail.com>
2026-06-30 10:53:37 +0530

    Refactor: Refactored the structure and cleaned up the files by adding a notes router

--- main.py
+++ main.py
@@ -1,57 +1,14 @@
-# main.py
-from fastapi import FastAPI, Path, Query, HTTPException
-from schema import NoteCreate, NoteResponse  
+from fastapi import FastAPI
+from routers.notes import router as notes_router
 
-app = FastAPI()
-
-notes = [
-    {"id": 1, "name": "Zap"}, 
-    {"id": 2, "name": "Apple"}, 
-    {"id": 3, "name": "Banana"}
-]
+app = FastAPI(Title = " My Organised FastAPI app")
 
 @app.get("/")
-def read():
-    return {"hello": "world"}
+def home():
+  return {"message": " Welcome to my API. Go to /docs to see the endpoints"}
 
-@app.post("/notes", response_model=NoteResponse)
-def write(note_input: NoteCreate):
-    new_id = len(notes) + 1
-
-    new_note = {"id": new_id, "name": note_input.name}
-    notes.append(new_note)
-
-    return new_note
-
-@app.patch("/notes/{note_id}", response_model=NoteResponse)
-def update_note(note_id: int, note_input: NoteCreate):
-    for note in notes:
-        if note["id"] == note_id:
-            note["name"] = note_input.name
-            return note
-            
-    raise HTTPException(status_code=404, detail="Note not found")
-
-@app.get('/notes/{note_id}', response_model=NoteResponse)
-def read_note(note_id: int = Path(..., description="Id of the note in the db")):
-    for note in notes:
-        if note["id"] == note_id:
-            return note
-    raise HTTPException(status_code=404, detail="Note not found.")
-
-@app.get('/sort')
-def sort_notes(sort_by: str = Query('name', description="Sort notes based on name"), order_by: str = Query('asc')):
-    if order_by not in ['asc', 'desc']:
-        raise HTTPException(status_code=400, detail="Invalid order_by value. Must be 'asc' or 'desc'.")
-    
-    is_reversed = True if order_by == 'desc' else False
-    
-    try:
-        sorted_notes = sorted(notes, key=lambda x: x[sort_by], reverse=is_reversed)
-        return sorted_notes
-    except KeyError:
-        raise HTTPException(status_code=400, detail=f"Cannot sort by invalid field: {sort_by}")
+app.include_router(notes_router)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+  import uvicorn
+  uvicorn.run("main:app", host = "0.0.0.0", port = 8000, reload = True)
---
+++ routers/notes.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter , Path, Query, HTTPException
+from schema import NoteCreate, NoteResponse  
+
+router = APIRouter(prefix ="/notes", tags = ["Notes"])
+
+notes = [
+    {"id": 1, "name": "Zap"}, 
+    {"id": 2, "name": "Apple"}, 
+    {"id": 3, "name": "Banana"}
+]
+
+@router.get("/", response_model=NoteResponse)
+def read_all_notes():
+    return notes
+
+@router.post("/", response_model=NoteResponse)
+def write_note(note_input: NoteCreate):
+    new_id = len(notes) + 1
+
+    new_note = {"id": new_id, "name": note_input.name}
+    notes.append(new_note)
+
+    return new_note
+
+@router.get('/{note_id}', response_model = NoteResponse)
+def read_note(note_id: int = Path(..., description="Id of the note in the db")):
+    for note in notes:
+        if note["id"] == note_id:
+            return note
+        else:
+            raise HTTPException(status_code = 404, detail="Note not found")
